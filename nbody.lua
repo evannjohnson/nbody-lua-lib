6e5716f63337904cbc6795bed13defbf7770a797
Evan Johnson <ev.jonnson@gmail.com>
2025-01-25 18:59:15 -0700

    initial commit

---
+++ nbody.lua
@@ -0,0 +1,22 @@
+-- nbody: nbody sim
+-- v0.1 @evannjohnson
+
+function init()
+  -- initialization
+end
+
+function key(n,z)
+  -- key actions: n = number, z = state
+end
+
+function enc(n,d)
+  -- encoder actions: n = number, d = delta
+end
+
+function redraw()
+  -- screen redraw
+end
+
+function cleanup()
+  -- deinitialization
+end
