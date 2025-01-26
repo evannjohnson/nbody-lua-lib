8ca1e817fda652b28331c7333880513b0cd6d0b0
Evan Johnson <ev.jonnson@gmail.com>
2025-01-26 14:11:13 -0700

    first steps

--- nbody.lua
+++ nbody.lua
@@ -2,21 +2,31 @@
 -- v0.1 @evannjohnson
 
 function init()
-  -- initialization
+    -- initialization
 end
 
-function key(n,z)
-  -- key actions: n = number, z = state
+function key(n, z)
+    -- key actions: n = number, z = state
 end
 
-function enc(n,d)
-  -- encoder actions: n = number, d = delta
+function enc(n, d)
+    -- encoder actions: n = number, d = delta
 end
 
 function redraw()
-  -- screen redraw
+    screen.clear()
+    screen.level(15)
+    screen.aa(0)
+    for i, body in ipairs(bodies) do
+        xpos = body.pos[1] / (max_xy * 1.2)
+        ypos = body.pos[3] / (max_xy * 1.2) / 2
+        screen.circle(xpos * 64 + 64, ypos * 32 + 32, 2)
+        screen.stroke()
+    end
+    screen.update()
 end
 
 function cleanup()
-  -- deinitialization
+    -- deinitialization
 end
+
