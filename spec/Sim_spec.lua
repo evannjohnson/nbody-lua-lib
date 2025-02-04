c2dd78fbd4641a0e2c8f71443c6eb744ca9b9555
Evan Johnson <ev.jonnson@gmail.com>
2025-02-04 15:50:17 -0700

    test 2 unmoving body momentum calculation

--- spec/Sim_spec.lua
+++ spec/Sim_spec.lua
@@ -4,14 +4,30 @@ local Vec2 = require("lib/vec2")
 
 describe("sim calculates center of momentum", function()
     it("with 1 body at origin", function()
-        local sim = Sim:new{
+        local sim = Sim {
             bodies = {
-                Body{
-                    pos = Vec2{0, 0},
-                    vel = Vec2{0, 0}
+                Body {
+                    pos = Vec2 { 0, 0 },
+                    vel = Vec2 { 0, 0 }
                 }
             }
         }
-        assert.are_same({0,0}, sim:getCenterOfMomentum())
+        assert.are_same({ 0, 0 }, sim:getCenterOfMomentum())
+    end)
+
+    it("with 2 unmoving bodies equidistant from the origin", function()
+        local sim = Sim {
+            bodies = {
+                Body {
+                    pos = Vec2 { 1, 1 },
+                    vel = Vec2 { 0, 0 }
+                },
+                Body {
+                    pos = Vec2 { -1, -1 },
+                    vel = Vec2 { 0, 0 }
+                }
+            }
+        }
+        assert.are_same({ 0, 0 }, sim:getCenterOfMomentum())
     end)
 end)
